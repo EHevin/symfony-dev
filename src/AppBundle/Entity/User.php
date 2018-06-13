c9381f1fb9af8006f1c977720b8a91a91dac579a
EHevin <erwan.hevin@gmail.com>
2018-06-13 17:02:25 +0200

    ajout nom + prenom bug

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -4,6 +4,7 @@ namespace AppBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity
@@ -17,6 +18,35 @@ class User extends BaseUser
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     *
+     * @Assert\NotBlank(message="Entrez votre nom.", groups={"Registration", "Profile"})
+     * @Assert\Length(
+     *     min=3,
+     *     max=255,
+     *     minMessage="The name is too short.",
+     *     maxMessage="The name is too long.",
+     *     groups={"Registration", "Profile"}
+     * )
+     */
+    protected $nom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     *
+     * @Assert\NotBlank(message="Entrez votre prenom.", groups={"Registration", "Profile"})
+     * @Assert\Length(
+     *     min=3,
+     *     max=255,
+     *     minMessage="Prenom trop court.",
+     *     maxMessage="Prenom trop long.",
+     *     groups={"Registration", "Profile"}
+     * )
+     */
+    protected $prenom;
+
 
     public function __construct()
     {
---
+++ src/AppBundle/Form/Type/RegistrationType.php
@@ -0,0 +1,38 @@
+<?php
+// src/AppBundle/Form/Type/RegistrationType.php
+
+namespace AppBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('nom');
+        $builder->add('prenom');
+    }
+
+    public function getParent()
+    {
+        return 'FOS\UserBundle\Form\Type\RegistrationFormType';
+
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'app_user_registration';
+    }
+
+    public function getNom()
+    {
+        return $this->getBlockPrefix();
+    }
+
+
+    public function getPrenom()
+    {
+        return $this->getBlockPrefix();
+    }
+}
